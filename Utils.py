622c1a79a1924286f586d222f6b5db35b22aa5bc
Mohammad Reza Samsami <39796257+mrsamsami@users.noreply.github.com>
2019-03-03 17:46:45 +0330

    Create Utils.py

---
+++ Utils.py
@@ -0,0 +1,76 @@
+import os
+import re
+import sys
+import json
+import math
+import time
+import unicodedata
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import function
+from tqdm import tqdm
+from functools import partial
+
+def shape_list(x):
+    """
+    deal with dynamic shape in tensorflow cleanly
+    """
+    ps = x.get_shape().as_list()
+    ts = tf.shape(x)
+    return [ts[i] if ps[i] is None else ps[i] for i in range(len(ps))]
+
+class ResultLogger(object):
+    def __init__(self, path, *args, **kwargs):
+        if 'time' not in kwargs:
+            kwargs['time'] = time.time()
+        self.f_log = open(make_path(path), 'w')
+        self.f_log.write(json.dumps(kwargs)+'\n')
+
+    def log(self, **kwargs):
+        if 'time' not in kwargs:
+            kwargs['time'] = time.time()
+        self.f_log.write(json.dumps(kwargs)+'\n')
+        self.f_log.flush()
+
+    def close(self):
+        self.f_log.close()
+
+def find_trainable_variables(key):
+    return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, ".*{}.*".format(key))
+
+def iter_data(*datas, n_batch=128, truncate=False, verbose=False, max_batches=float("inf")):
+    n = len(datas[0])
+    if truncate:
+        n = (n//n_batch)*n_batch
+    n = min(n, max_batches*n_batch)
+    n_batches = 0
+    if verbose:
+        f = sys.stderr
+    else:
+        f = open(os.devnull, 'w')
+    for i in tqdm(range(0, n, n_batch), total=n//n_batch, file=f, ncols=80, leave=False):
+        if n_batches >= max_batches: raise StopIteration
+        if len(datas) == 1:
+            yield datas[0][i:i+n_batch]
+        else:
+            yield (d[i:i+n_batch] for d in datas)
+        n_batches += 1
+
+@function.Defun(
+    python_grad_func=lambda x, dy: tf.convert_to_tensor(dy),
+    shape_func=lambda op: [op.inputs[0].get_shape()])
+def convert_gradient_to_tensor(x):
+    """force gradient to be a dense tensor
+    it's often faster to do dense embedding gradient on GPU than sparse on CPU
+    """
+    return x
+
+def assign_to_gpu(gpu=0, ps_dev="/device:CPU:0"):
+    def _assign(op):
+        node_def = op if isinstance(op, tf.NodeDef) else op.node_def
+        if node_def.op == "Variable":
+            return ps_dev
+        else:
+            return "/gpu:%d" % gpu
+    return _assign
+  
